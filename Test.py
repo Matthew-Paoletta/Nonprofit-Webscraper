ddff3cb7ad40a01d137a6819d1b128baa9c44b6a
Matthew Vincent Paoletta <mvppvm42@gmail.com>
2025-04-22 16:41:01 -0700

    Here is the current attempts to make this work
    Failing

---
+++ Test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com")
+
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("Selenium with VS Code")
+search_box.submit()
+
+time.sleep(5) 
+driver.quit()  
